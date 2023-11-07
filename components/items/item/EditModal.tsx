f454f4b8a399b96348b330f841b06ac2312ebed4
ligulfzhou <ligulfzhou53@gmail.com>
2023-11-07 15:25:44 +0800

    items fields

--- components/items/item/EditModal.tsx
+++ components/items/item/EditModal.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useEffect, useState} from "react";
-import {Modal, Form, Input, message, DatePicker, Select} from "antd";
+import {Modal, Form, Input, message, DatePicker, Select, InputNumber} from "antd";
 import useSWRMutation from "swr/mutation";
 import {Item} from "@/types/item";
 import {updateItem, UpdateItemParam} from "@/requests/item";
@@ -23,15 +23,15 @@ const EditModal: FC<Props> = (
     const onFinish = (values: UpdateItemParam) => {
         let id = 0
         if (obj) {
-            id= obj.id
+            id = obj.id
         }
 
         values['id'] = id
         console.log(values)
 
-        callUpdateAPI(values).then((res)=> {
-            if (res.code==0) {
-                message.success(`${obj?"修改": "添加"}成功`)
+        callUpdateAPI(values).then((res) => {
+            if (res.code == 0) {
+                message.success(`${obj ? "修改" : "添加"}成功`)
                 closeFn(true)
                 form.resetFields()
             } else {
@@ -44,18 +44,18 @@ const EditModal: FC<Props> = (
 
     useEffect(() => {
         let _formValues: UpdateItemParam = {
-            barcode: obj?.barcode||'',
-            brand: obj?.brand||'',
-            buy_price: obj?.buy_price||'',
-            cates1: obj?.cates1||'',
-            cates2: obj?.cates2||'',
-            color: obj?.color||'',
-            goods_no: obj?.goods_no||'',
-            name: obj?.name||'',
-            sell_price: obj?.sell_price||'',
-            size: obj?.size||'',
-            unit: obj?.unit||'',
-            id: obj?.id||0
+            barcode: obj?.barcode || '',
+            brand: obj?.brand || '',
+            buy_price: obj?.buy_price || '',
+            cates1: obj?.cates1 || '',
+            cates2: obj?.cates2 || '',
+            color: obj?.color || '',
+            goods_no: obj?.goods_no || '',
+            name: obj?.name || '',
+            sell_price: obj?.sell_price || '',
+            size: obj?.size || '',
+            unit: obj?.unit || '',
+            id: obj?.id || 0
         }
         setFormValues(_formValues)
         form.setFieldsValue(_formValues)
@@ -72,7 +72,7 @@ const EditModal: FC<Props> = (
                 width={700}
                 open={open}
                 centered={true}
-                title={`${obj? "修改": "添加"}产品`}
+                title={`${obj ? "修改" : "添加"}产品`}
                 onCancel={(e) => {
                     e.preventDefault()
                     form.resetFields()
@@ -95,6 +95,13 @@ const EditModal: FC<Props> = (
                             label="产品名"
                             name="name"
                             rules={[{required: true, message: '请输入产品名!'}]}
+                        >
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="英文名"
+                            name="english_name"
                         >
                             <Input/>
                         </Form.Item>
@@ -138,42 +145,55 @@ const EditModal: FC<Props> = (
 
                     <div className='grid grid-cols-2 mt-6'>
                         <Form.Item
-                            label="客户类型"
-                            name="ty_pe"
-                            rules={[{required: true, message: '请选择客户类型!'}]}
+                            label="售价"
+                            name="sell_price"
                         >
-                            <Select
-                                style={{ width: 200 }}
-                                loading
-                                options={
-                                    [
-                                        {
-                                            label: '请选择',
-                                            value: '',
-                                        },
-                                        {
-                                            label: '普通客户',
-                                            value: 1,
-                                        },
-                                        {
-                                            label: 'VIP客户',
-                                            value: 2,
-                                        }
-                                    ]}
-                            />
+                            <InputNumber style={{width: 180}}/>
                         </Form.Item>
-
                         <Form.Item
-                            label="地址"
-                            name="address"
+                            label="进货价"
+                            name="buy_price"
+                        >
+                            <InputNumber style={{width: 180}}/>
+                        </Form.Item>
+                    </div>
+
+                    <div className='grid grid-cols-2 mt-6'>
+                        <Form.Item
+                            label="品牌"
+                            name="brand"
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="供应商"
+                            name="supplier"
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="材质"
+                            name="material"
                         >
                             <Input/>
                         </Form.Item>
 
                         <Form.Item
-                            label="客户编号"
-                            name="customer_no"
-                            rules={[{required: true, message: '请输入客户编号!'}]}
+                            label="件数(PCS)"
+                            name="pcs"
+                        >
+                            <InputNumber/>
+                        </Form.Item>
+                        <Form.Item
+                            label="重量"
+                            name="weight"
+                        >
+                            <InputNumber/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="描述"
+                            name="description"
                         >
                             <Input/>
                         </Form.Item>
@@ -181,7 +201,6 @@ const EditModal: FC<Props> = (
                         <Form.Item
                             label="备注"
                             name="notes"
-                            rules={[{required: false, message: '请输入备注!'}]}
                         >
                             <Input/>
                         </Form.Item>
